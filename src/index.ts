12713cdcb0b97d8f0ef652961bec7bdb439ebd93
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-10-19 15:05:35 +0200

    interface

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+
+interface Results {
+    winner: string;
+    date: Date;
+
+    getWinner(): String;
+    setWinner(winner: string): void;
+
+    getDate(): Date;
+    setDate(date: Date): void;
+
+  result(): String;
+    
+}
